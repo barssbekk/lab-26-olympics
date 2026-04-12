7904ab2715b1356ee5f22cadbd452da64f88f231
Barsbek Azamatov <bars.moderside@gmail.com>
2026-04-12 16:55:07 -0700

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 using namespace chrono;
 
-const int NUM_RUNS{5}; // repeat tests so time is noticeable
+const int NUM_RUNS{20}; // repeat tests so time is noticeable
 const int NUM_SIM{15};
 const int NUM_OPER{4};
 const int NUM_DS{3};
@@ -51,7 +51,7 @@ int main() {
 
     long results[NUM_SIM][NUM_OPER][NUM_DS]{}; // 3D array to store results
 
-    cout << "Running";
+    cout << "Running\n";
     for (auto& result : results) {
         // read
         result[0][0] = inputVector(data);
@@ -73,8 +73,11 @@ int main() {
         result[3][1] = deleteList(dataList);
         result[3][2] = deleteSet(dataSet);
     }
-    cout << "Test\n"
-         << results[0][0][0] << '\n';
+    cout << "Test: \n";
+    for (int i{0}; i < NUM_SIM; ++i) {
+        cout << "Run " << i + 1 << ": "
+             << results[i][0][0] << "\n";
+    }
 
     long vecRead {inputVector(data)};
     long listRead {inputList(data)};
